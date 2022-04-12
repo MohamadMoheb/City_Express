97c65ed3d1a1bc682147c2df440a03a06ff82bb8
Mohamad Moheb <mohamadmohebx@gmail.com>
2022-04-12 07:33:20 +0300

    Create script.js

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+function password_show_hide() {
+    var x = document.getElementById("password");
+    var show_eye = document.getElementById("show_eye");
+    var hide_eye = document.getElementById("hide_eye");
+    hide_eye.classList.remove("d-none");
+    if (x.type === "password") {
+      x.type = "text";
+      show_eye.style.display = "none";
+      hide_eye.style.display = "block";
+    } else {
+      x.type = "password";
+      show_eye.style.display = "block";
+      hide_eye.style.display = "none";
+    }
+  }
